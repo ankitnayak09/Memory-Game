32f96952954af37a736ac58a7875c841973f0f82
Ankit Kumar Nayak <68335227+ankitnayak09@users.noreply.github.com>
2023-12-12 19:17:09 +0530

    Update styles and add new game button and Add Assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-	const [count, setCount] = useState(0);
-
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<h1>Magic Match</h1>
+			<button>New Game</button>
+		</div>
+	);
 }
 
 export default App;
